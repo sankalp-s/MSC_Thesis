bee4d1eef14913fb228fc33038f36fd937aa81e9
Sankalp Arora <10sankalp.arora@gmail.com>
2024-03-23 05:48:08 +0000

    Update rominfo.py
    Updates rom.info

--- Player_Inputs/Final/rominfo.py
+++ Player_Inputs/Final/rominfo.py
@@ -3,23 +3,23 @@ from itertools import product
 
 
 '''
-Extração de atributos da memória RAM do jogo Super Mario World
-Informações retiradas de: https://www.smwcentral.net/?p=nmap&m=smwram
+Extraction of attributes from the RAM memory of the game Super Mario World
+Information taken from: https://www.smwcentral.net/?p=nmap&m=smwram
 '''
 def getXY(ram):
     '''
-    getXY(ram): retorna informação da posição do agente
-    embora layer1? não seja utilizada no momento, pode ser útil em algumas 
-    alterações do algoritmo de aprendizado.
+    getXY(ram): returns information about the agent's position
+    although layer1? is not currently used, it may be useful in some
+    changes to the learning algorithm.
     '''
     
-    # Coordenadas x, y em relação a fase inteira 
-    # Elas estão armazenadas em 2 bytes cada
-    # no formato little endian
+    # Coordinates x, y relative to the entire level
+    # They are stored in 2 bytes each
+    # in little endian format
     marioX = ram[0x95]*256 + ram[0x94]
     marioY = ram[0x97]*256 + ram[0x96]
     
-    # Coordenada da parte visível do site
+    # Coordinate of the visible part of the level
     layer1x = ram[0x1B]*256 + ram[0x1A]
     layer1y = ram[0x1D]*256 + ram[0x1C]
     
@@ -27,27 +27,27 @@ def getXY(ram):
 
 def getSprites(ram):
   '''
-  getSprites(ram): retorna os sprites (blocos, inimigos, itens) exibidos na tela.
+  getSprites(ram): returns the sprites (blocks, enemies, items) displayed on the screen.
   '''
   
   sprites = []
   extsprites = []
   
-  # Podem existir até 12 sprites na tela
+  # There can be up to 12 sprites on the screen
   for slot in range(12):
-    # se o status é 0, não tem sprite nesse slot
+    # if the status is 0, there is no sprite in this slot
     status = ram[0x14C8+slot]
     if status != 0:
-      # posição x,y do sprite
+      # x,y position of the sprite
       spriteX    = ram[0xE4+slot] + ram[0x14E0+slot]*256
       spriteY    = ram[0xD8+slot] + ram[0x14D4+slot]*256
       
-      spriteSize = ram[0x0420+ram[0x15EA+slot]]  # tamanho do sprite
-      spriteId   = ram[0x15EA+slot]              # qual é o sprite?
+      spriteSize = ram[0x0420+ram[0x15EA+slot]]  # sprite size
+      spriteId   = ram[0x15EA+slot]              # which sprite is it?
       
-      # se for item (44) ou bloco ? (216), não insere nas informações
+      # if it's an item (44) or block? (216), don't include it in the information
       if spriteId != 44 and spriteId != 216:
-        # ou ele é 1x1 ou 4x4 blocos da nossa janela
+        # either it's 1x1 or 4x4 blocks in our window
         size = 1
         if spriteSize == 0:
           size = 4
@@ -57,37 +57,36 @@ def getSprites(ram):
       
 def getTile(dx,dy,ram):
   '''
-  getTile(dx, dy, ram): retorna se tem um bloco que o mario possa pisar na posição dx, dy
+  getTile(dx, dy, ram): returns whether there is a block the Mario can step on at position dx, dy
   '''
   x = np.floor(dx/16)
   y = np.floor(dy/16)
   
-  # 0x1C800 indica para cada pixel se é um obstáculo ou não
-  # como obter o ponto certo foi retirado daqui: https://www.smwcentral.net/?p=viewthread&t=78887
+  # 0x1C800 indicates for each pixel whether it is an obstacle or not
+  # how to get the right point was taken from here: https://www.smwcentral.net/?p=viewthread&t=78887
   # return ram[0x1C800 + int(np.floor(x/16)*432 + y*16 + x%16)]
 
-  # O endereço correto é 0x1F000, contribuição de Fernando Teixeira
+  # The correct address is 0x1F000, contribution by Fernando Teixeira
   if (0x1F000 + int(np.floor(x / 16) * 432 + y * 16 + x % 16)) > 131071:
-
     return ram[131071]
   else:
     return ram[0x1F000 + int(np.floor(x / 16) * 432 + y * 16 + x % 16)]
   
 def getInputs(ram, radius=6):
   '''
-  getInputs(ram): retorna uma nd.array de inimigos, obstáculos dentro de um raio em torno do agente
+  getInputs(ram): returns an nd.array of enemies, obstacles within a radius around the agent
   '''
   
   marioX, marioY, layer1x, layer1y = getXY(ram)
   sprites = getSprites(ram)
 
-  # tamanho do vetor
+  # vector length
   maxlen = (radius*2+1)*(radius*2+1)
   inputs = np.zeros(maxlen, dtype=int)
   
-  # cada bloco de imagem representa 16x16 pixesl
-  # portanto tendo um x,y de referência do Mario
-  # devemos camihar de 16 em 16
+  # each image block represents 16x16 pixels
+  # so having a reference x,y from Mario
+  # we should walk 16 at a time
   window = (-radius*16, radius*16 + 1, 16)
   j = 0
 
@@ -95,30 +94,30 @@ def getInputs(ram, radius=6):
     return (idx%(2*r + 1) + ds2 < 2*r + 1) and (idx + ds1*(2*r + 1) + ds2 < maxlen)
   
   for dy, dx in product(range(*window), repeat=2):
-    # verifica se tem obstáculo na posição x+dx, y+dy
-    # o +8 é para começar a medir a partir do meio do Mario
+    # checks if there is an obstacle at position x+dx, y+dy
+    # the +8 is to start measuring from the middle of Mario
     tile = getTile(marioX+dx+8, marioY+dy, ram)
     
-    # O Mario está sempre no meio, 
-    # deve checar se o y está dentro do limite
+    # Mario is always in the middle, 
+    # it must check if y is within the limit
     if tile==1 and marioY+dy < 0x1B0:
       inputs[j] = 1
     
-    # Para cada sprite  
+    # For each sprite  
     for i in range(len(sprites)):
-      # Se estiver dentro do bloco de 16 x 16 (-8, +8)
+      # If it's within the 16 x 16 block (-8, +8)
       distx = np.abs(sprites[i]['x'] - marioX - dx)
       disty = np.abs(sprites[i]['y'] - marioY - dy)
       size = sprites[i]['size']
       if distx <= 8 and disty <= 8:
-        # se estiver dentro dos limites, insira -1
+        # if it's within the limits, insert -1
         for s1, s2 in product(range(size), repeat=2):
           if withinLimits(j, s1, s2, radius, maxlen):
             inputs[j + s1*(radius*2 + 1) + s2] = -1
     j = j + 1
   return inputs, marioX, marioY
   
-# Recupera o estado atual como uma string
+# Retrieves the current state as a string
 def getState(ram, radius):
   state, x, y = getInputs(ram, radius=radius)
   return ','.join(map(str,state)), x, y  
@@ -129,5 +128,4 @@ def getRam(env):
         ram += list(v)
     return np.array(ram)
     #return np.array(list(env.data.memory.blocks[8257536]))
-    
-
+ 
