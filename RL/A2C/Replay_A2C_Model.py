247c3a3fefcda26b8e5d502a93d7958156855b9a
Sankalp Arora <10sankalp.arora@gmail.com>
2024-04-12 02:11:10 +0100

    Create Replay_A2C_Model.py

---
+++ RL/A2C/Replay_A2C_Model.py
@@ -0,0 +1,30 @@
+import retro
+from baselines.common.retro_wrappers import *
+from stable_baselines3 import DQN,PPO,A2C
+from stable_baselines3.common.policies import obs_as_tensor
+import time
+import numpy as np
+from baselines.common.atari_wrappers import *
+
+env = retro.make(game = "SuperMarioWorld-Snes",use_restricted_actions=retro.Actions.DISCRETE,state = 'YoshiIsland2',obs_type = retro.Observations.IMAGE)
+
+#Frame skip (hold an action for this many frames) and sticky actions
+env = StochasticFrameSkip(env,4,0.25)
+#scale and turn RGB image to grayscale
+env = WarpFrame(env,width=84,height=84,grayscale=True)
+blank_action = 0
+
+model = PPO.load("A2C_IMAGE_L1.zip")
+
+obs = env.reset()
+
+while True:
+    action, _states = model.predict(obs, deterministic=False)
+    
+    obs, reward, done, info = env.step(action)
+    obs, rew, done, info = env.step(blank_action)#Stop it from holding an action, like jump
+    
+    print(env.get_action_meaning(action))
+    env.render()
+    if done:
+      obs=env.reset()
